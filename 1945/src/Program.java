e6e19011401b226d44c04fa409628ac6976649d5
Hoàng Trần Hảo <tranhaockpt@gmail.com>
2016-11-30 22:16:12 +0700

    Initial Commit

---
+++ 1945/src/Program.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by tranh on 30-Nov-16.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+        GameWindow gameWindow=new GameWindow();
+    }
+}
